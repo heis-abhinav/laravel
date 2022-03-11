56266a15f9c2763f1a71860770d9230a1cfff7f0
Abhinav Sharma <abhinavkumr77@gmail.com>
2022-03-11 12:12:33 +0530

    pizzacontroller modification to read from model

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -3,20 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Pizza;
 class PizzaController extends Controller
 {
     public function index(){
-        $pizzas = [
-        ['type'=>'hawaiin','base'=>'cheesy crust','price'=> 15],
-        ['type'=>'volcano','base'=>'garlic crust','price'=> 25],
-        ['type'=>'veg supreme','base'=>'thin and crispy','price'=> 10],
-    ];
+        $pizzas = Pizza::all();
 
     $name = request('name');
     return view('pizzas',[
         'pizzas' => $pizzas,
-        'name' => $name]);
+        ]);
     }
 
     public function show($id){
