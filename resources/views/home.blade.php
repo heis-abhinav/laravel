e3a3dfab09004b4b5a915db86a6e57e02e9683c6
abhinav <abhinavkumr77@gmail.com>
2022-03-25 12:15:24 +0530

    new commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+@include('pagecontent.products')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+
+                    @endif
+
+                    @auth
+                        {{ __('You are logged in!') }}
+                    @endauth
+                </div>
+            </div>
+            @yield('products')
+        </div>
+
+
+            
+    </div>
+</div>
+@endsection
