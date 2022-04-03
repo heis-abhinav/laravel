81e3a1ed652bf34746cc612cd61a037110e604e8
abhinav <abhinavkumr77@gmail.com>
2022-04-03 13:00:59 +0530

    admin vue file

---
+++ resources/js/App.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <div class="container">
+         
+        <side-component />
+        <div id="main">
+            <button class="btn" v-on:click="openNav" title="Open Sidebar">☰</button>
+            <router-view ></router-view>
+        </div>
+    </div>
+
+</template>
+<script type="text/javascript">
+    import panelmethods from './adminpanel'
+    export default {
+            methods: {
+                openNav : function ()
+                {
+                    panelmethods.openNav();
+                     
+                },
+            }
+        
+    }
+
+</script>
+
+<style type="text/css">
+    #main {
+      transition: margin-left .5s;
+      padding: 16px;
+    }
+</style>
