02465c2b2f2fcbacba36c8ece1a0d1bbfa92319c
abhinav <abhinavkumr77@gmail.com>
2022-04-03 12:59:15 +0530

    new folder for store view

---
+++ resources/views/store/pagecontent/products.blade.php
@@ -0,0 +1,9 @@
+@section('products')
+<div class="card">
+    <div class="card-header">{{ __('Product') }}</div>
+
+    <div class="card-body">
+    	<h1>this is product</h1>
+    </div>
+</div>
+@endsection
