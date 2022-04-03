7347017fd40d8e111b54cc07e6f08ebaf9ac5c53
abhinav <abhinavkumr77@gmail.com>
2022-04-03 13:16:33 +0530

    Store vue file

---
+++ resources/js/Store.vue
@@ -0,0 +1,12 @@
+<template>
+
+    <div class="container">
+         
+        <side-component />
+        <div id="navigate">
+            <h1>navigation</h1>
+            <router-view ></router-view>
+        </div>
+    </div>
+
+</template>
