fef4fb5ed5e33cdd0bb338777c2edcc336f6310c
abhinav <abhinavkumr77@gmail.com>
2022-04-03 12:54:34 +0530

    Methods enhancement

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -16,7 +16,26 @@ class ItemsController extends Controller
     }
 
     public function add_item_page(){
-        return view('items.add');
+        return view('admin.add');
+    }
+
+    public function view_items_page(){
+        return view('admin.viewitems');
+    }
+
+    public function view_items(){
+        $items = Items::orderBy('id', 'desc')
+            ->get()
+            ->toArray();
+        return response()->json($items);
+    }
+    public function view_item_page(){
+        return view('admin.viewitem');
+    }
+    public function view_item($id){
+        $item = Items::findOrFail($id)->toArray();
+        return response()->json($item);
+        
     }
 
     public function store(Request $request){
@@ -45,5 +64,39 @@ class ItemsController extends Controller
                 'message' => 'Item registered successfully'
             ]
         );
+    }
+
+    public function update(Request $request, $id){
+        if($request->avatar){
+
+            $image_name = time().'.' . explode('/', explode(':', substr($request->avatar, 0, strpos($request->avatar, ';')))[1])[1];
+            \Image::make($request->avatar)->save(public_path('img/').$image_name);
+            $request->merge(['avatar' => $image_name]);
+           
+        }
+
+        $item = array(
+                    'name' => $request->name,
+                    'description' => $request->description,
+                    'price' => $request->price,
+                    'tags' => $request->tags,
+                    'quantity' => $request->quantity);
+        // $item->name = $request->name;
+        // $item->description = $request->description;
+        // $item->price = $request->price;
+        // //$item->image = $image_name;
+        // $item->tags = $request->tags;
+        // $item->quantity = $request->quantity;
+        Items::where('id', $id)->update($item);
+        return response()->json(
+            [
+                'success' => true,
+                'message' => 'Item updated successfully'
+            ]
+        );
+    }
+
+    function view_admin_dashboard(){
+        return view('admin.dashboard');
     }
 }
