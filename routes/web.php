cac32803f507b1217f817d75f9ef08b28b433c66
abhinav <abhinavkumr77@gmail.com>
2022-04-03 13:09:52 +0530

    routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-//use App\Http\Controllers\Restaurant\AuthRestaurantController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('store.home');
 });
 
 Auth::routes();
@@ -22,5 +21,12 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-Route::get('/add-item', [App\Http\Controllers\ItemsController::class, 'add_item_page'])->name('add-item')->middleware('auth');
-Route::post('/store',[App\Http\Controllers\ItemsController::class, 'store']);
+Route::get('/admin/add-item', [App\Http\Controllers\ItemsController::class, 'add_item_page'])->name('add-item')->middleware('auth');
+Route::post('/admin/storeitem',[App\Http\Controllers\ItemsController::class, 'store']);
+Route::post('/admin/updateitem/{id}',[App\Http\Controllers\ItemsController::class, 'update']);
+Route::get('/admin/items', [App\Http\Controllers\ItemsController::class , 'view_items_page'])->name('view-items');
+Route::get('/admin/viewitems', [App\Http\Controllers\ItemsController::class , 'view_items']);
+Route::get('/admin/viewitem/{id}', [App\Http\Controllers\ItemsController::class , 'view_item']);
+Route::get('/admin/item/{id}', [App\Http\Controllers\ItemsController::class , 'view_item_page']);
+
+Route::get('/admin/dashboard', [App\Http\Controllers\ItemsController::class , 'view_admin_dashboard']);
