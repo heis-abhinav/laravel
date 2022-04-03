47434c91b8f54dc3a24e763a54e673a0fa3cbde7
abhinav <abhinavkumr77@gmail.com>
2022-04-03 13:13:24 +0530

    admin js files

---
+++ resources/js/adminroutes.js
@@ -0,0 +1,31 @@
+import ItemComponent from './components/admin/ItemComponent.vue';
+import EditComponent from './components/admin//EditComponent.vue';
+import FileUpload from './components/admin//FileUpload.vue';
+import AdminDashboard from './components/admin/AdminDashboard.vue';
+export const adminroutes = [
+    {
+        name: 'items',
+        path: '/admin/items',
+        component: ItemComponent
+    },
+    // {
+    //     name: 'create',
+    //     path: '/create',
+    //     component: CreateComponent
+    // },
+    {
+        name: 'edit',
+        path: '/admin/item/:id',
+        component: EditComponent
+    },
+    {
+        name: 'add',
+        path: '/admin/add-item',
+        component: FileUpload
+    },
+    {
+        name: 'dashboard',
+        path: '/admin/dashboard',
+        component: AdminDashboard
+    },
+];
