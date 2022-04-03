ebb4d6b32987aa667b7c41c8672788bad2123679
abhinav <abhinavkumr77@gmail.com>
2022-04-03 13:05:20 +0530

    Files removed

--- resources/views/items/add.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div id="app">
-<file-upload-component></file-upload-component>
-</div>
-@endsection
--- resources/views/pagecontent/products.blade.php
+++
@@ -1,9 +0,0 @@
-@section('products')
-<div class="card">
-    <div class="card-header">{{ __('Product') }}</div>
-
-    <div class="card-body">
-    	<h1>this is product</h1>
-    </div>
-</div>
-@endsection
